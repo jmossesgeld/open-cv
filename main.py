67ae41f5fb5c223730b6fae7bab340009409ecf7
jmossesgeld <jmossesgeld@gmail.com>
2022-02-17 21:50:42 +0800

    update tutorial

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import numpy
+import cv2
+import numpy as np
 
-numpy.set_printoptions(precision=3, suppress=True)
-print("Hello World!")
+
+def draw_circle(e, x, y, flags, param):
+    if cv2.EVENT_LBUTTONDOWN == e:
+        cv2.circle(img, (x, y), 100, (255, 255, 255))
+        cv2.imshow("image", img)
+
+
+img = np.zeros((512, 512, 3))
+cv2.namedWindow("image")
+cv2.imshow("image", img)
+cv2.waitKey(0)
